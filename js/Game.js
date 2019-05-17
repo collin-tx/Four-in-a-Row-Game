f9301789b0af6c364d5b764fe460696ddb7c6a8b
Collin Miller Smith <44204853+collin-tx@users.noreply.github.com>
2019-05-17 12:00:53 -0500

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,21 @@
 class Game {
+    constructor() {
+        this.board = new Board;
+        this.players = createPlayers();
+        this.ready = false;
+    }
 
+    /** 
+     * Creates two player objects
+     * @return  {Array}    An array of two Player objects.
+     */
+
+    createPlayers() {
+        const players = [new Player("Player 1", "#e15258", 1, true),
+            new Player("Player 2", "#e59a13", 2)
+        ];
+        return players;
+    }
+
+    startGame() {};
 }

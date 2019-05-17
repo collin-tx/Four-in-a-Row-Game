cd7f24961c5266a412b779cbc436075f7989b21e
Collin Miller Smith <44204853+collin-tx@users.noreply.github.com>
2019-05-17 13:21:07 -0500

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -4,21 +4,30 @@ class Player {
             this.id = id;
             this.color = color;
             this.active = active;
-            this.tokens = createTokens(21);
+            this.tokens = this.createTokens(21);
         }
         /**
          * Creates token objects for player
          * @param     {integer}    num - Number of token objects to be created
          * @returns   {Array}     An array of the newly created token objects
          */
-   
-    createTokens(num) {
-        const tokens = [];
 
-        for (let i = 0; i < num; i++) {
-            let token = new Token(i, this);
-            tokens.push(token);
+    createTokens(num) {
+            const tokens = [];
+
+            for (let i = 0; i < num; i++) {
+                let token = new Token(i, this);
+                tokens.push(token);
+            }
+            return tokens;
         }
-        return tokens;
+        // gets tokens that have yet to be played
+    get unusedTokens() {
+            return this.tokens.filter(token => !token.dropped);
+        }
+        // gets active token 
+    get activeToken() {
+        return this.tokens[0];
     }
+
 }

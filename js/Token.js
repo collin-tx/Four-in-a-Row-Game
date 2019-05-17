2eb0a87bdace4fae4d8cfa051ab57def68a69998
Collin Miller Smith <44204853+collin-tx@users.noreply.github.com>
2019-05-17 13:23:37 -0500

    Update Token.js

--- js/Token.js
+++ js/Token.js
@@ -1,7 +1,21 @@
 class Token {
-    constructor() {
+    constructor(index, owner) {
         this.owner = owner;
         this.id = `token-${index}-${owner.id}`;
         this.dropped = false;
+    }
+
+    // gets associated token
+    get HTMLToken() {
+        return document.getElementById(this.id);;
+    }
+
+    // draws new HTML token
+    drawHTMLToken() {
+        const token = document.createElement('div');
+        document.getElementById('game-board-underlay').appendChild(token);
+        token.setAttribute('id', this.id);
+        token.setAttribute('class', "token");
+        token.style.backgroundColor = this.owner.color;
     }
 }

dd1ef59493ab82ee56a4a41f4850a29c6a87ef4c
Collin Miller Smith <44204853+collin-tx@users.noreply.github.com>
2019-05-18 17:00:21 -0500

    Update Token.js

--- js/Token.js
+++ js/Token.js
@@ -3,6 +3,7 @@ class Token {
         this.owner = owner;
         this.id = `token-${index}-${owner.id}`;
         this.dropped = false;
+        this.columnLocation = 0;
     }
 
     // gets associated token
@@ -17,5 +18,36 @@ class Token {
         token.setAttribute('id', this.id);
         token.setAttribute('class', "token");
         token.style.backgroundColor = this.owner.color;
+    }
+
+    // gets left offset of html element
+    get offsetLeft() {
+            return this.HTMLToken.offsetLeft;
+
+        }
+        //moves token one column to the left
+    moveLeft() {
+            if (this.columnLocation > 0) {
+                this.HTMLToken.style.left = this.offsetLeft - 76;
+                this.columnLocation -= 1;
+            }
+        }
+        //moves token one column to the right
+    moveRight(columns) {
+        if (this.columnLocation < columns - 1) {
+            this.HTMLToken.style.left = this.offsetLeft + 76;
+            this.columnLocation += 1;
+        }
+
+    }
+
+    //plays active token
+    drop(target, reset) {
+        this.dropped = true;
+
+        $(this.HTMLToken).animate({
+            top: (target.y * target.diameter)
+        }, 750, 'easeOutBounce', reset);
+
     }
 }
